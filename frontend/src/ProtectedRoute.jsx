1dd79a6bee1d4b6f5ad501123f0069be94000c5e
hinahondotdev <hinahondev@gmail.com>
2025-10-20 05:32:59 +0800

    Delete frontend directory

--- frontend/src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
-
-export default function ProtectedRoute({ children }) {
-  const { user, loading } = useAuth();
-  if (loading) return <div style={{padding:20}}>Loading...</div>;
-  return user ? children : <Navigate to="/" replace />;
-}
--- frontend/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// src/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-// replace these with your actual values from Supabase project settings
-const SUPABASE_URL = "https://qqksytpofkppluxjrywk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxa3N5dHBvZmtwcGx1eGpyeXdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NDU0MTUsImV4cCI6MjA3MzIyMTQxNX0.GVxyFLbksFzuRbizN7ck5AWtpRlRqlJompQfXm3wJLE";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
